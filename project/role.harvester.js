9039179b82148ebca698245bb0782a0f64cbfbbb
Mary Tionko <mary.tionko@gmail.com>
2023-08-07 11:59:31 +1200

    Bumps tower down in restock priority

--- project/role.harvester.js
+++ project/role.harvester.js
@@ -12,8 +12,7 @@ var roleHarvester = {
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType === STRUCTURE_EXTENSION ||
-                                structure.structureType === STRUCTURE_SPAWN ||
-                                structure.structureType === STRUCTURE_TOWER) && 
+                                structure.structureType === STRUCTURE_SPAWN) && 
                                 structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
@@ -23,12 +22,27 @@ var roleHarvester = {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             } else {
-                var closestContainer = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                    filter: (structure) => structure.structureType === STRUCTURE_CONTAINER && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-                })
-                if(closestContainer) {
-                    if(creep.transfer(closestContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(closestContainer, {visualizePathStyle: {stroke: '#ffffff'}});
+                var closestTower = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return structure.structureType === STRUCTURE_TOWER && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+                    }
+                });
+                if(closestTower) {
+                    if(creep.transfer(closestTower, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(closestTower, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                } else {
+                    var closestContainer = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                        filter: (structure) => structure.structureType === STRUCTURE_CONTAINER && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+                    })
+                    if(closestContainer) {
+                        if(creep.transfer(closestContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(closestContainer, {visualizePathStyle: {stroke: '#ffffff'}});
+                        }
+                    } else {
+                        // Could not find anywhere to store energy, give up
+                        creep.say('Dropping');
+                        creep.drop(RESOURCE_ENERGY);
                     }
                 }
             }
