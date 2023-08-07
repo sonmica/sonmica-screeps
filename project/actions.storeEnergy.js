ac544b6e9ac3562befe65b55300b83c67c777908
Mary Tionko <mary.tionko@gmail.com>
2023-08-07 14:18:01 +1200

    Bump tower priority down even more

--- project/actions.storeEnergy.js
+++ project/actions.storeEnergy.js
@@ -13,28 +13,20 @@ var storeEnergy = {
                 creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
             }
         } else {
-            var closestTower = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return structure.structureType === STRUCTURE_TOWER && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-                }
+            var closestTowerOrContainer = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: (structure) => (
+                    structure.structureType === STRUCTURE_TOWER
+                    || structure.structureType === STRUCTURE_CONTAINER
+                ) && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
             });
-            if(closestTower) {
-                if(creep.transfer(closestTower, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(closestTower, {visualizePathStyle: {stroke: '#ffffff'}});
+            if(closestTowerOrContainer) {
+                if(creep.transfer(closestTowerOrContainer, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(closestTowerOrContainer, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             } else {
-                var closestContainer = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                    filter: (structure) => structure.structureType === STRUCTURE_CONTAINER && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-                })
-                if(closestContainer) {
-                    if(creep.transfer(closestContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(closestContainer, {visualizePathStyle: {stroke: '#ffffff'}});
-                    }
-                } else {
-                    // Could not find anywhere to store energy, give up
-                    creep.say('Dropping');
-                    creep.drop(RESOURCE_ENERGY);
-                }
+                // Could not find anywhere to store energy, give up
+                creep.say('Dropping');
+                creep.drop(RESOURCE_ENERGY);
             }
         }
     }
