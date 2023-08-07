b452d334582233a9840d1ff133d5bbfc72959193
Mary Tionko <mary.tionko@gmail.com>
2023-08-07 15:04:59 +1200

    Cleans up carrier actions

--- project/role.carrier.js
+++ project/role.carrier.js
@@ -20,21 +20,7 @@ var roleCarrier = {
                     creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
                 } else {
                     creep.say("Storing");
-                    var targets = creep.room.find(FIND_STRUCTURES, {
-                            filter: (structure) => {
-                                return (structure.structureType === STRUCTURE_EXTENSION ||
-                                        structure.structureType === STRUCTURE_SPAWN ||
-                                        structure.structureType === STRUCTURE_TOWER ||
-                                        structure.structureType === STRUCTURE_CONTAINER) && 
-                                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                            }
-                    });
-                    // TODO: priority ordering
-                    if(targets.length > 0) {
-                        if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                        }
-                    }
+                    actions.storeEnergy(creep);
                 }
             }
         }
